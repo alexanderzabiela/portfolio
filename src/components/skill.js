7b92438a46ad68efeccbe76ac51717410a7e55c1
Alexander Zabiela <alexanderzabiela@gmail.com>
2019-09-02 12:56:45 +0100

    fixed bug which was making text overlap

--- src/components/skill.js
+++ src/components/skill.js
@@ -20,6 +20,8 @@ const Skill = styled('div')`
   p {
     font-size: 13px;
     width: 240px;
+    padding-left: 12px;
+    padding-right: 12px;
   }
 
   @media only screen and (min-width: 768px) {
